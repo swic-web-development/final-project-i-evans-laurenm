c2d404066dcff51fbbe03d4e616f3f7da504296a
Lauren Evans <lauren.evans@student.swic.edu>
2025-05-16 00:53:08 -0500

    Add actions file:  fetch person from SWAPI

---
+++ src/actions.jsx
@@ -0,0 +1,20 @@
+import store from './store.js'
+
+export async function fetchPeople() {
+  store.setState({ isLoading: true, error: null })
+
+  try {
+    const res = await fetch('https://www.swapi.tech/api/people/3/')
+    const data = await res.json()
+
+    const person = data.result?.properties
+
+    const detailedPeople = person ? [person] : []
+
+    console.log('Detailed People:', detailedPeople)
+
+    store.setState({ people: detailedPeople, isLoading: false })
+  } catch (err) {
+    store.setState({ error: err.message, isLoading: false, people: [] })
+  }
+}
